b6c885e2ecfb7a8b393b75cc6917e55f60bb7b28
Luis Herdeth <luis.herdeth@gmail.com>
2023-01-02 12:25:11 +0100

    Completed exercice 3

---
+++ ejercicio3/ejercicio3.js
@@ -0,0 +1,62 @@
+/**----------------------
+ **   BUCLE FOR
+ *@param factorial num
+ *@return valor
+ *------------------------**/
+function getFactorial(num) {
+	let valor = 1;
+	for (let i = 1; i <= num; i++) {
+		valor = valor * i;
+	}
+	return valor;
+}
+console.log(getFactorial(15));
+
+/**-----------------------
+ * *       BUCLE WHILE
+ *
+ *
+ *------------------------**/
+
+function factorialWhile(num) {
+	let i = 1;
+	let valor = 1;
+
+	while (i <= num) {
+		valor = valor * i;
+		i++;
+	}
+	return valor;
+}
+
+console.log(factorialWhile(4));
+
+/**=======================
+ * *      getFactorialByStatement
+ *
+ *
+ *========================**/
+
+// console.log(valor);
+function getFactorialByStatement(num) {
+	let i = 1;
+	let valor = 1;
+	while (true) {
+		if (i > num) {
+			break;
+		}
+		valor *= i;
+		i++;
+
+        // other
+        
+		// if (i <= 4) {
+		//   valor = valor * i;
+		//   i++;
+		// } else {
+		//   break;
+		// }
+	}
+	return valor;
+}
+console.log(getFactorialByStatement(10));
