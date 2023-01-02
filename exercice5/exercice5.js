1a21f1bbabb2c42b75a991094c84c66c6a667d0e
Luis Herdeth <luis.herdeth@gmail.com>
2023-01-02 19:09:44 +0100

    Finished exercice 5

---
+++ exercice5/exercice5.js
@@ -0,0 +1,14 @@
+/**=======================
+ * *       EXERCICE 4
+ *
+ *
+ *========================**/
+
+console.log(`${parseInt(170 * 100)}cm`);
+console.log(`${parseFloat(170 * 100)}m`);
+console.log(Math.ceil(1.7));
+console.log(Math.floor(69.6));
+// console.log(parseFloat(70));
+console.log(Math.round(1.7));
+console.log(Math.round(69.6));
+console.log(Number.MAX_SAFE_INTEGER + 1 === Number.MAX_SAFE_INTEGER + 2);
