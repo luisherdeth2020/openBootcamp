cfc7aebe4b599a827982c7b6bff6ce626d984e44
Luis Herdeth <luis.herdeth@gmail.com>
2023-01-02 15:24:45 +0100

    Rename directory

---
+++ ejercicio2/ejercicio2.js
@@ -0,0 +1,13 @@
+const ejercicio2 = {
+	name        : 'Luis Herdeth',
+	age         : 32,
+	developer   : false,
+	date        : new Date(1990, 2, 26),
+	favoriteBook: {
+		title : 'Bob esponja',
+		author: 'Emilio Mc',
+		date  : new Date(2001, 1, 18),
+		url   : 'https://luisherdeth.com/',
+	},
+};
+console.log(ejercicio2.favoriteBook);
