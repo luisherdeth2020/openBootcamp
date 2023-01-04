53a7f88d075c71f89a7681fb45bc309872c1c226
Luis Herdeth <luis.herdeth@gmail.com>
2023-01-04 09:11:24 +0100

    Finished 7.4 (prompt)

---
+++ exercici7/about_blank.js
@@ -0,0 +1,14 @@
+/**----------------------------------------------
+ * *                   7.4 PROMPT GOOGLE CHROME
+ *   
+ *   
+ *   
+ *
+ *---------------------------------------------**/
+
+//Abre una nueva ventana "about:blank" en Google Chrome
+
+//Abre la consola de desarrollador de Google Chrome (F12)
+
+//Pregunta al usuario cuál es su edad y almacénala en una variable llamada edad
+prompt('¿Qué edad tienes?');
