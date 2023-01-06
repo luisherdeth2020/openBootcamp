2eafa6b13e830aeff0206afadcb8d862367251ee
Luis Herdeth <luis.herdeth@gmail.com>
2023-01-06 22:26:39 +0100

    Finished exercice 17

--- exercice16/index.js
+++ exercice16/index.js
@@ -1,40 +1,40 @@
 const parrafo = document.querySelectorAll('.parrafo');
 const secciones = document.querySelectorAll('.section');
-const clearParrafo = document.querySelectorAll('.clear--parrafo');
 
 parrafo.forEach((parrafo) => {
-	//inicio del arrastre
 	parrafo.addEventListener('dragstart', (event) => {
 		console.log('inicio de arrastre');
-		// console.log(parrafo.innerText);
+
 		parrafo.classList.add('dragging');
 		event.dataTransfer.setData('id', parrafo.id);
 	});
-	parrafo.addEventListener('dragend', (event) => {
+	parrafo.addEventListener('dragend', () => {
 		console.log('He terminado de arrastre');
 		parrafo.classList.remove('dragging');
-
-		event.dataTransfer.setData('id', parrafo.id);
 	});
 });
 
 secciones.forEach((seccion) => {
 	seccion.addEventListener('dragover', (event) => {
-		console.log('drag over');
 		event.preventDefault();
 	});
 
 	seccion.addEventListener('drop', (event) => {
 		const id_parrafo = event.dataTransfer.getData('id');
-		console.log('parrafo id: ' + id_parrafo);
 
 		const parrafo = document.getElementById(id_parrafo);
 		seccion.appendChild(parrafo);
-		// const borrar = event.dataTransfer.clearData();
-
-		if (parrafo == clearParrafo) {
-			// parrafo.parentNode.removeChild(parrafo);
-			parrafo.remove(parrafo);
-		}
 	});
+});
+
+const clearParrafo = document.querySelector('.clear--parrafo');
+
+clearParrafo.addEventListener('dragover', (event) => {
+	event.preventDefault();
+});
+
+clearParrafo.addEventListener('drop', (event) => {
+	const id_parrafo = event.dataTransfer.getData('id');
+	const parrafo = document.getElementById(id_parrafo);
+	parrafo.remove(id_parrafo);
 });
