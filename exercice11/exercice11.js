997732c751500da2780400bb73a93a5b40af9e65
Luis Herdeth <73970555+luisherdeth2020@users.noreply.github.com>
2023-01-07 09:57:55 +0100

    Finished exercice 11

--- exercice11/exercice11.js
+++ exercice11/exercice11.js
@@ -1,25 +1,26 @@
 /**----------------------------------------------
  * *                   EXERCICE 11
- *   
- *   
- *   
+ *
+ *
+ *
  *
  *---------------------------------------------**/
 
 class Student {
 	constructor(name) {
 		// Una variable llamada nombre
-		this.name=name;
+		this.name = name;
 		// Otra variable lista llamada asignaturas con 3 asignaturas: Javascript, HTML, CSS
 		this.subjets = ['Javascript', 'Html', 'Css'];
 	}
 	// Un método "obtenDatos" que devuelva un objeto con las propiedades nombre y asignaturas
 	getData() {
-		return this.name, this.subjets;
+		// return this.name, this.subjets
+		return `${this.name} conoce los lenguajes ${this.subjets}`;
 	}
 }
 // Crea una nueva instancia de "Estudiante"
-const newStudent = new Student();
+const newStudent = new Student('Luis Herdeth');
 
 // Haz la llamada al método obtenDatos
 console.log(newStudent.getData());
